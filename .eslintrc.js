57a6f43f894b8ec0df4f3fe0576c24d85038a9c9
Kimmo Tukiainen <kimmo@featly.fi>
2018-03-11 22:16:45 +0200

    Lint nuked the project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-    "extends": ["airbnb", "prettier"]
+    "extends": ["airbnb", "prettier"],
+    "globals": {
+        "describe": true,
+        "it": true,
+        "expect": true
+    }
 };
